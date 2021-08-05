a8f5cfc17da0cf56cd8a6f3e6c6b8cfa2b14e070
Jimmy Briggs <jimmy.briggs@jimbrig.com>
2021-08-05 19:37:42 -0400

    add gcp compute

---
+++ GCP/Compute-Network-Simple/main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "3.5.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = file("defaultproject-default-service-account-key.json")
+
+  project = "defaultproject-321020"
+  region  = "us-east1"
+  zone    = "us-east1-b"
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
+}
